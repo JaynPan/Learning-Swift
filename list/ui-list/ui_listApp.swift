c82942abc2c808bcf53aa4e05efd3253692709e9
Jay Pan <lb0104672@gmail.com>
2023-06-14 19:51:48 +0800

    list-ui

---
+++ list/ui-list/ui_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  ui_listApp.swift
+//  ui-list
+//
+//  Created by 潘傑恩 on 2023/6/14.
+//
+
+import SwiftUI
+
+@main
+struct ui_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VideoListView()
+        }
+    }
+}
