c92b3fd344414fec136d3766c9a02b66f8616e15
Jay Pan <lb0104672@gmail.com>
2023-06-28 23:23:25 +0800

    call API request

---
+++ networkCall/network/network/networkApp.swift
@@ -0,0 +1,17 @@
+//
+//  networkApp.swift
+//  network
+//
+//  Created by 潘傑恩 on 2023/6/28.
+//
+
+import SwiftUI
+
+@main
+struct networkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
